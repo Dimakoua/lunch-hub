27e89c93cce5aea8558c06b784f0fdd88ce11fa2
Dmytro Kozin <dimakoua@gmail.com>
2025-09-07 17:10:29 -0400

    feat: add i18n support with English and Spanish translations

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,17 @@ import App from './App.tsx';
 import './index.css';
 import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
+import './i18n'; // Import the i18n configuration
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
       <BrowserRouter>
-        <App />
+        <I18nextProvider i18n={i18n}>
+          <App />
+        </I18nextProvider>
       </BrowserRouter>
     </HelmetProvider>
   </StrictMode>
